1121e538506428cc9bb8950d69f4241d349e7611
Viet Nguyen <vietanh.nguyen@wsu.edu>
2021-09-11 13:18:31 -0700

    Fixed grid bug
    no need for grid-gap. Added footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Body from './components/Nav/Body';
+import Footer from './components/UI/Footer';
 
 function App() {
   return (
     <div className="App">
      <Body />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/UI/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+      <footer>
+        <p>Viet Nguyen 2021</p>
+      </footer>
+    );
+}
+
+export default Footer
