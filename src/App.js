dd9dda2a5d7870c81adacd102e7784123ec17908
Viet Nguyen <vietanh.nguyen@wsu.edu>
2021-09-12 19:35:48 -0700

    update search form
    beginning search form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Body from './components/Nav/Body';
 import Footer from './components/UI/Footer';
 
+//https://rickandmortyapi.com/documentation/#filter-characters
+
 function App() {
   return (
     <div className="App">
--- src/components/Forms/ChararacterSearchForm.js
+++ src/components/Forms/ChararacterSearchForm.js
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ChararacterSearchForm = () => {
+    const [name, setName] = useState('')
+    const [status, setStatus] = useState(null);
+    const [species, setSpecies] = useState(null);
+
     return (
-        <div style={{textAlign: `center`, padding: `1% 5%`}}>
-            <form>
-                <input type='text' />
-                <button type='submit'>Search</button>
-            </form>
-        </div>
-    )
+      <div style={{ textAlign: `center`, padding: `1% 5%` }}>
+        <form>
+          <div>
+            <label>Name </label>
+            <input
+              value={name}
+              type="text"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <label>Status: </label>
+            <select>
+              <option value="alive">Alive</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
+          <div>
+            <label>Species: </label>
+            <select>
+              <option value="alive">Alive</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
+          <div>
+            <label>Type: </label>
+            <select>
+              <option value="alive">Alive</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
+          <div>
+            <label>Gender: </label>
+            <select>
+              <option value="alive">male</option>
+              <option value="alive">Female</option>
+              <option value="alive">Genderless</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
 }
 
 export default ChararacterSearchForm
